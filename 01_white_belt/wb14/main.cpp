c276c1e32cdad133243d4e9990dd85a024d5ce00
Michael <micek.mk@mail.ru>
2023-01-10 00:59:44 +0300

    1.4 - Задание по программированию «A + B»

---
+++ 01_white_belt/wb14/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Задание по программированию «A + B»
+ * На вход программе через стандартный ввод передаются два целых числа, по модулю не превышающие 100000. Выведите в
+ * стандартный вывод их сумму.
+ * https://stepik.org/lesson/283164/step/9?auth=login&unit=264468 *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  return 0;
+}
